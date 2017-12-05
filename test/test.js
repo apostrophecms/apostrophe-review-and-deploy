09fda281cc6f68dfbb2c4df132a8d29bd87545f8
Tom Boutell <tom@punkave.com>
2017-12-05 14:45:53 -0500

    Passing unit tests for join _id patching

--- test/test.js
+++ test/test.js
@@ -5,7 +5,8 @@ describe('apostrophe-site-review', function() {
   var apos;
   var apos2;
   var exported;
-
+  var oldAboutId;
+  
   this.timeout(5000);
 
   after(function() {
@@ -22,7 +23,28 @@ describe('apostrophe-site-review', function() {
       
       modules: {
         'apostrophe-pages': {
-          park: [],
+          park: [
+            {
+              slug: '/test',
+              type: 'testPage',
+              navigation: {
+                type: 'area',
+                items: [
+                  {
+                    type: 'navigation',
+                    _id: 'xyz',
+                    by: 'id',
+                    ids: [ 'placeholder' ]
+                  }
+                ]
+              }
+            },
+            {
+              slug: '/about',
+              type: 'testPage',
+              title: 'About'
+            }
+          ],
           types: [
             {
               name: 'home',
@@ -63,7 +85,30 @@ describe('apostrophe-site-review', function() {
 
   it('should export a locale on request', function() {
     var req = apos.tasks.getReq({ locale: 'fr' });
-    return apos.modules['apostrophe-site-review'].exportLocale(req)
+
+    // First patch the join in the parked pages so that it
+    // works in the original locale, that way we can test
+    // later that it works after the import and the
+    // ids have in fact been remapped
+    return apos.docs.db.findOne({
+      workflowLocale: 'fr',
+      slug: '/about'
+    })
+    .then(function(about) {
+      assert(about);
+      oldAboutId = about._id;
+      return apos.docs.db.update({
+        workflowLocale: 'fr',
+        slug: '/test'
+      }, {
+        $set: {
+          'navigation.items.0.ids.0': oldAboutId
+        }
+      });
+    })
+    .then(function() {
+      return apos.modules['apostrophe-site-review'].exportLocale(req);
+    })
     .then(function(filename) {
       assert(filename);
       var fs = require('fs');
@@ -78,9 +123,30 @@ describe('apostrophe-site-review', function() {
     return apos.modules['apostrophe-site-review'].importLocale(req, exported)
     .then(function() {
       return apos.docs.db.count({ workflowLocale: 'frimporttest' })
-      .then(function(n) {
-        assert(n > 0);
+    })
+    .then(function(n) {
+      assert(n > 0);
+      // Great, but did the remapping of join ids work?
+      return apos.docs.db.findOne({
+        workflowLocale: 'frimporttest',
+        slug: '/about'
+      })
+    })
+    .then(function(_about) {
+      about = _about;
+      assert(about._id !== oldAboutId);
+      return apos.docs.db.findOne({
+        workflowLocale: 'frimporttest',
+        slug: '/test'
       });
+    })
+    .then(function(test) {
+      assert(test.navigation);
+      assert(test.navigation.items);
+      assert(test.navigation.items[0]);
+      assert(test.navigation.items[0].ids);
+      assert(test.navigation.items[0].ids[0] !== oldAboutId);
+      assert(test.navigation.items[0].ids[0] === about._id);
     });
   });
 });
